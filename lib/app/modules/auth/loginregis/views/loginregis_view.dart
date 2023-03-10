822bf0dddd0dd875de6c831487795a55775f5483
Ridoca33 <127484603+Ridoca33@users.noreply.github.com>
2023-03-10 15:54:51 +0700

    Update loginregis_view.dart

--- lib/app/modules/auth/loginregis/views/loginregis_view.dart
+++ lib/app/modules/auth/loginregis/views/loginregis_view.dart
@@ -12,6 +12,7 @@ class LoginregisView extends GetView<LoginregisController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
